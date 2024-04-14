2a29717be9e1b6aeabe7aa81db8c10fa4f73645b
Bryan Su <bsuia00@gmail.com>
2024-04-14 17:48:39 -0500

    Completed pthread implementation

--- hw4/3way-pthread/pthread_imp.c
+++ hw4/3way-pthread/pthread_imp.c
@@ -5,19 +5,47 @@
 
 #define NUM_THREADS 4
 
-#define LINE_COUNT 1000000
+#define NUM_LINES 1000000
+#define LINE_LENGTH 2500
 
 pthread_mutex_t mutex;          // mutex for line_max_ascii
-int line_max_ascii[LINE_COUNT];
-const int line_count_per_thread = (int)(LINE_COUNT / NUM_THREADS);
+char data[NUM_LINES][LINE_LENGTH];
+int line_max_ascii[NUM_LINES];
 
-FILE *stream;
+int line_count = 0;
+int line_count_per_thread = 0;
+int line_left_over = 0;
 
 void init_array()
 {
-    for (int i = 0; i < LINE_COUNT; i++) {
+    for (int i = 0; i < NUM_LINES; i++) {
         line_max_ascii[i] = 0;
     }
+}
+
+int read_file(const char* file_name)
+{
+    FILE *stream;
+    line_count = 0;
+
+    stream = fopen(file_name, "r");
+    if (stream == NULL) {
+        printf("ERROR; Unable to open %s\n", file_name);
+        return -1;
+    }
+
+    ssize_t nread;
+    char *line = NULL;
+    size_t len = 0;
+    while ((nread = getline(&line, &len, stream)) != -1) {
+        memcpy(data[line_count], line, nread);
+        line_count++;
+    }
+
+    fclose(stream);
+    line_count_per_thread = (int)(line_count / NUM_THREADS);
+    line_left_over = line_count % NUM_THREADS;
+    return 0;
 }
 
 int get_max_ascii_val(char* line)
@@ -43,6 +71,9 @@ void *process_line(void *myID)
 
     int startPos = ((int) myID) * line_count_per_thread;
     int endPos = startPos + line_count_per_thread;
+    if ((int)myID == NUM_THREADS - 1) {
+        endPos += line_left_over;
+    }
 
     printf("myID = %d startPos = %d endPos = %d \n", (int) myID, startPos, endPos);
 
@@ -54,7 +85,7 @@ void *process_line(void *myID)
     // Get the max ascii value for each line
     for (int i = startPos; i < endPos; i++) {
         // get the line i
-        local_line_max_ascii[i - startPos] = get_max_ascii_val(/*line i*/);
+        local_line_max_ascii[i - startPos] = get_max_ascii_val(data[i]);
     }
 
     // Put the values in the global array
@@ -67,21 +98,27 @@ void *process_line(void *myID)
     pthread_exit(NULL);
 }
 
+void print_results() {
+    for (int i = 0; i < line_count; i++) {
+        printf("%i: %i\n", i, line_max_ascii[i]);
+    }
+}
+
 main(int argc, char **argv) 
 {
-    if (argc < 1) {
+    if (argc < 2) {
         printf("%s <text file>\n", argv[0]);
         exit(-1);
     }
 
     const char* text_file_name = argv[1];
 
-    FILE *stream;
-    stream = fopen(text_file_name, "r");
-    if (stream == NULL) {
-        printf("ERROR; Unable to open %s\n", text_file_name);
+    init_array();
+    if (read_file(text_file_name) == -1) {
         exit(-1);
     }
+
+    printf("Line Count: %i, Line Count Per Thread: %i, Leftovers: %i\n", line_count, line_count_per_thread, line_left_over);
 
     pthread_t threads[NUM_THREADS];
     pthread_attr_t attr;
@@ -90,8 +127,6 @@ main(int argc, char **argv)
     /* Initialize and set thread detached attribute */
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
-    init_array();
 
     for (int i = 0; i < NUM_THREADS; i++ ) {
         int rc = pthread_create(&threads[i], &attr, process_line, (void *)i);
@@ -111,11 +146,9 @@ main(int argc, char **argv)
 	    }
 	}
 
-    // TODO: print results 0:99, 1:101, ...
+    print_results();
 
-    
     pthread_mutex_destroy(&mutex);
 	printf("Main: program completed. Exiting.\n");
 	pthread_exit(NULL);
-    fclose(stream);
 }
