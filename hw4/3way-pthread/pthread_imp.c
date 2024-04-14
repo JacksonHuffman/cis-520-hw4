867628d233cd9a48fdf34dbe6325210267a39043
Bryan Su <bsuia00@gmail.com>
2024-04-14 15:22:30 -0500

    Redid the pthread implementation

--- hw4/3way-pthread/pthread_imp.c
+++ hw4/3way-pthread/pthread_imp.c
@@ -7,8 +7,16 @@
 
 #define LINE_COUNT 1000000
 
-pthread_mutex_t mutex;
+pthread_mutex_t mutex;          // mutex for line_max_ascii
 int line_max_ascii[LINE_COUNT];
+const int line_count_per_thread = (int)(LINE_COUNT / NUM_THREADS);
+
+void init_array()
+{
+    for (i = 0; i < LINE_COUNT; i++) {
+        line_max_ascii[i] = 0;
+    }
+}
 
 int get_max_ascii_val(char* line)
 {
@@ -29,20 +37,29 @@ int get_max_ascii_val(char* line)
 
 void *process_line(void *myID)
 {
-    //char theChar;
-    int i;
-    //int local_char_count[ALPHABET_SIZE];
+    int local_line_max_ascii[line_count_per_thread];
 
-    int startPos = ((int) myID) * (LINE_COUNT / NUM_THREADS);
-    int endPos = startPos + (LINE_COUNT / NUM_THREADS);
+    int startPos = ((int) myID) * line_count_per_thread;
+    int endPos = startPos + line_count_per_thread;
 
     printf("myID = %d startPos = %d endPos = %d \n", (int) myID, startPos, endPos);
 
+    // init local line max ascii
+    for (int i = 0; i < line_count_per_thread; i++) {
+        local_line_max_ascii[i] = 0;
+    }
 
-    pthread_mutex_lock (&mutex);
-    for (i = startPos; i < endPos; i++) {
+    // Get the max ascii value for each line
+    for (int i = startPos; i < endPos; i++) {
         // get the line i
-        line_max_ascii[i] = get_max_ascii_val(/*line i*/);
+        local_line_max_ascii[i - startPos] = get_max_ascii_val(/*line i*/);
+    }
+
+    // Put the values in the global array
+    pthread_mutex_lock (&mutex);
+    for (int i = startPos; i < endPos; i++) {
+        // get the line i
+        line_max_ascii[i] = local_line_max_ascii[i - startPos];
     }
     pthread_mutex_unlock (&mutex);
 
@@ -59,21 +76,21 @@ main(int argc, char **argv)
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
     for (i = 0; i < NUM_THREADS; i++ ) {
-	      rc = pthread_create(&threads[i], &attr, process_line, (void *)i);
-	      if (rc) {
-	        printf("ERROR; return code from pthread_create() is %d\n", rc);
-		exit(-1);
-	      }
+        rc = pthread_create(&threads[i], &attr, process_line, (void *)i);
+        if (rc) {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
 	}
 
     /* Free attribute and wait for the other threads */
 	pthread_attr_destroy(&attr);
 	for(i=0; i<NUM_THREADS; i++) {
-	     rc = pthread_join(threads[i], &status);
-	     if (rc) {
-		   printf("ERROR; return code from pthread_join() is %d\n", rc);
-		   exit(-1);
-	     }
+	    rc = pthread_join(threads[i], &status);
+	    if (rc) {
+            printf("ERROR; return code from pthread_join() is %d\n", rc);
+            exit(-1);
+	    }
 	}
 
     // TODO: print results 0:99, 1:101, ...
