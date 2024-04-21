19b19718a4766950fbde632666868b57531ec2c0
Jackson Huffman <jdhuffman02@ksu.edu>
2024-04-21 16:42:22 -0500

    Testing pt2.c

--- hw4/pt2/pt2.c
+++ hw4/pt2/pt2.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 //#define NUM_THREADS 4
 int NUM_THREADS;
@@ -14,6 +15,8 @@ char char_array[ARRAY_SIZE][STRING_SIZE];
 int char_counts[ALPHABET_SIZE];			// global count of individual characters
 int local_char_count[ALPHABET_SIZE];
 
+int line_count = 0;
+
 char getRandomChar()
 {
 	int randNum = 0;
@@ -25,6 +28,28 @@ char getRandomChar()
 
 	// printf("%c", randChar);
 	return randChar;
+}
+
+int read_file(const char *file_name) {
+    FILE *stream;
+    line_count = 0;
+
+    stream = fopen(file_name, "r");
+    if (stream == NULL) {
+        printf("ERROR: Unable to open %s\n", file_name);
+        return -1;
+    }
+
+    ssize_t nread;
+    char *line = NULL;
+    size_t len = 0;
+    while ((nread = getline(&line, &len, stream)) != -1) {
+        memcpy(char_array[line_count], line, nread);
+        line_count++;
+    }
+
+    fclose(stream);
+    return 0;
 }
 
 void init_arrays()
@@ -87,6 +112,7 @@ main(int argc, char* argv[])
 	int numtasks, rank;
 	MPI_Status Status;
 
+	const char* text_file_name = argv[1];
 
 	rc = MPI_Init(&argc,&argv);
 	if (rc != MPI_SUCCESS) {
@@ -102,8 +128,12 @@ main(int argc, char* argv[])
 	fflush(stdout);
 
 	if ( rank == 0 ) {
-		init_arrays();
+		init_array();
+        if (read_file(text_file_name) == -1) {
+            exit(-1);
+        }
 	}
+
 	MPI_Bcast(char_array, ARRAY_SIZE * STRING_SIZE, MPI_CHAR, 0, MPI_COMM_WORLD);
 		
 	count_array(&rank);
